7c34803c01bd9b252d593b0adc67939a6408fca1
Michael Gesenhues <88671780+mgese@users.noreply.github.com>
2025-10-31 13:46:17 +0100

    feat: add liquid glass input

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Components
 export { default as ColorSchemeProvider} from './components/color-scheme-provider/ColorSchemeProvider'
+export { default as LiquidGlassInput, type LiquidGlassInputProps, type LiquidGlassInputRef } from './components/liquid-glass-input'
 
 // Types
 export {ColorMode, type Colors, type Theme} from './components/color-scheme-provider/ColorSchemeProvider.types'
